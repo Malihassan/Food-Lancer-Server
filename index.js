92978042edde94b90a3a69687ded4261f61dd34c
Malihassan <momenalhendawy@gmail.com>
2022-02-10 14:41:06 +0200

    add axpress and database connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongo = require('mongoose')
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+mongo.connect(process.env.ATLS_URL,()=>{
+    console.log('connected to database');
+})
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`listen on port ${process.env.PORT}`);
+})
