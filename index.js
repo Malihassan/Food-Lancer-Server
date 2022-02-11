aaa3a9c60766e0e18475f949b4e030d33b79c783
MarwaShehata <engmarwaadel95@gmail.com>
2022-02-11 17:28:57 +0200

    added product schema

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const express = require("express");
-const mongo = require('mongoose')
+const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-mongo.connect(process.env.ATLS_URL,()=>{
+mongoose.connect(process.env.ATLS_URL,()=>{
     console.log('connected to database');
 })
-
 app.use(cors())
 app.use(express.json())
-
-
-
-
 app.listen(process.env.PORT,()=>{
     console.log(`listen on port ${process.env.PORT}`);
 })
