0b127d84a8b347077b8e1d64056742891454a321
abramBoutros <63019144+abramBoutros@users.noreply.github.com>
2022-02-13 22:06:40 +0200

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const chatSchema = mongoose.Schema(
 	{
 		buyerId: {
-			type: mongo.SchemaTypes.ObjectId,
+			type: mongoose.SchemaTypes.ObjectId,
 			ref: "buyer",
 			required: true,
 		},
 		sellerId: {
-			type: mongo.SchemaTypes.ObjectId,
+			type: mongoose.SchemaTypes.ObjectId,
 			ref: "seller",
 			required: true,
 		},
@@ -29,5 +29,5 @@ const chatSchema = mongoose.Schema(
 	{ timeStamp: true }
 );
 
-const ChatModel = mongo.model("Chat", chatSchema);
+const ChatModel = mongoose.model("Chat", chatSchema);
 module.exports = ChatModel;
