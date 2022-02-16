f3b15f1d6d49a1f9aaa76d2f1f274a1ab0731b11
Malihassan <momenalhendawy@gmail.com>
2022-02-17 01:56:49 +0200

    add account router (seller/account)

--- routers/seller/index.js
+++ routers/seller/index.js
@@ -1,49 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const accountController = require("../../controllers/seller/account");
-const AppError = require("../../helpers/ErrorClass");
+const router = require("express").Router();
 const productRouter = require("./product");
-const sellerAuthentication = require("../../middleware/sellerAuth");
+const accountRouter = require('./account')
 
 router.use("/product", productRouter);
-router.post("/login", async (req, res, next) => {
-	const { email, password } = req.body;
-	try {
-		if (!email) return next(new AppError("allFieldsRequired"));
-		if (!password) return next(new AppError("allFieldsRequired"));
-
-		const token = await accountController.login(email, password);
-		res.json({ token });
-	} catch (error) {
-		next(error);
-	}
-});
-
-router.patch(
-	"/editProfile/:id",
-	sellerAuthentication,
-	async function (req, res) {
-		const { phone, password, firstName, lastName, coverageArea } = req.body;
-		const { id } = req.seller;
-
-		const seller = await accountController.updateSeller(
-			id,
-			phone,
-			password,
-			firstName,
-			lastName,
-			coverageArea
-		);
-		if (!seller) {
-			res.status(404).send("seller not found");
-		}
-		if (seller === "No Area") {
-			res.statusMessage = "This area is not being covered";
-			res.status(404).json(seller);
-		}
-		res.statusMessage = "seller data updated successfully";
-		res.status(200).json(seller);
-	}
-);
+router.use("/account", accountRouter)
 
 module.exports = router;
