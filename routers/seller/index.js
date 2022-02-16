edef7b8d8962a837172c4bae0e6bd9766c5e5d7f
Abram Boutros <abramboutros96@gmail.com>
2022-02-16 20:55:44 +0200

    renamed a variable

--- routers/seller/index.js
+++ routers/seller/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../../controllers/seller/account");
 const AppError = require("../../helpers/ErrorClass");
-const productRouter=require('./product');
-router.use('/product',productRouter);
+const productRouter = require("./product");
+router.use("/product", productRouter);
 router.post("/login", async (req, res, next) => {
 	const { email, password } = req.body;
 	try {
@@ -20,7 +20,7 @@ router.post("/login", async (req, res, next) => {
 router.patch("/editProfile/:id", async function (req, res) {
 	const { phone, password, firstName, lastName, coverageArea } = req.body;
 
-	const user = await accountController.updateSeller(
+	const seller = await accountController.updateSeller(
 		req.params.id,
 		phone,
 		password,
@@ -28,15 +28,15 @@ router.patch("/editProfile/:id", async function (req, res) {
 		lastName,
 		coverageArea
 	);
-	if (!user) {
+	if (!seller) {
 		res.status(404).send("seller not found");
 	}
-	if (user === "No Area") {
+	if (seller === "No Area") {
 		res.statusMessage = "This area is not being covered";
-		res.status(404).json(user);
+		res.status(404).json(seller);
 	}
 	res.statusMessage = "seller data updated successfully";
-	res.status(201).json(user);
+	res.status(201).json(seller);
 });
 
 module.exports = router;
