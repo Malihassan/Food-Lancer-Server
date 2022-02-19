22be40c963a3e432f95503828240cd4060e2c207
Abram Boutros <abramboutros96@gmail.com>
2022-02-19 20:09:00 +0200

    new admin routes created + admin login

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const router = express.Router()
-const seller = require('./seller/index');
-const buyer = require('./buyer/index');
-// router.use('/admin',admins)
-router.use('/seller', seller);
-router.use('/', buyer);
-router.get('/',(req, res) => {
-  res.status(200).json({ message: 'Connected!' });
+const express = require("express");
+const router = express.Router();
+const seller = require("./seller/index");
+const buyer = require("./buyer/index");
+const admin = require("./admin/admin");
+
+router.use("/seller", seller);
+router.use("/", buyer);
+router.get("/", (req, res) => {
+	res.status(200).json({ message: "Connected!" });
 });
 
-module.exports =router
+router.use("/admin", admin);
+
+module.exports = router;
