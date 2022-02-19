b0626b3883338cd7ab8f631d2741ede93829e912
Abram Boutros <abramboutros96@gmail.com>
2022-02-19 20:23:53 +0200

    commented buyer in routes index.js

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const seller = require("./seller/index");
-const buyer = require("./buyer/index");
+// const buyer = require("./buyer/index");
 const admin = require("./admin/index");
 
 router.use("/seller", seller);
-router.use("/", buyer);
+// router.use("/", buyer);
 router.get("/", (req, res) => {
 	res.status(200).json({ message: "Connected!" });
 });
