dda36b3aa97c76c7ae485f9acc5bec5051799577
Abram Boutros <abramboutros96@gmail.com>
2022-02-20 16:30:19 +0200

    added adminAuth to middleware

---
+++ middleware/adminAuth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const AppError = require("../helpers/ErrorClass");
+const AdminModel = require("../models/admin");
+require("dotenv").config();
+
+async function adminAuth(req, res, next) {
+	try {
+		const { token } = req.headers;
+		const payload = jwt.verify(token, process.env.SECRETKEY);
+		const admin = await AdminModel.findById(payload.id);
+		if (!admin) {
+			return next(new AppError("accountNotFound"));
+		}
+		req.admin = admin;
+		next();
+	} catch (error) {
+		next(new AppError("JsonWebTokenError"));
+	}
+}
+module.exports = adminAuth;
