596761b0242c7e482b3d37d974c76b19be18a3f4
Malihassan <momenalhendawy@gmail.com>
2022-03-01 23:35:37 +0200

    fixed confilect in  admin router of  seller

--- routers/admin/seller.js
+++ routers/admin/seller.js
@@ -3,18 +3,10 @@ const router = express.Router();
 const sellerController = require("../../controllers/seller");
 const productController = require('../../controllers/product')
 const adminAuthentication = require("../../middleware/adminAuth");
-<<<<<<< HEAD
 
 
 router.get('/allSellers',adminAuthentication,sellerController.getAllSellers)
 router.get("/:status", adminAuthentication, sellerController.getSellers);
-=======
-router.get(
-  "/allSellers",
-  adminAuthentication, 
-  sellerController.getallSellers
-);
->>>>>>> 0c6c006d17fdfba8103f041ea61c7b5f97d2ab5b
 /* updateProductForSpecifcSeller */
 router.get(
   "/all/:status",
@@ -41,7 +33,7 @@ router.get(
 // router.patch("/update/:id", sellerController.updateSeller);
 
 //   /:id/orders
-);
+// );
 router.patch("/update/:id", adminAuthentication,sellerController.updateSeller);
 
 router.get("/:id/orders", adminAuthentication,sellerController.getOrdersForSpecificSeller);
