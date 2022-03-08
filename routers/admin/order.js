fcf411328b588158796700f71cd376ca1428eebd
Abram Boutros <abramboutros96@gmail.com>
2022-03-08 19:10:06 +0200

    combined getAllOrders and getOrdersForSpecificQuery into getOrders

--- routers/admin/order.js
+++ routers/admin/order.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const orderController = require("../../controllers/order");
-const adminAuth=require('../../middleware/adminAuth');
+const adminAuth = require("../../middleware/adminAuth");
 
-router.get("/allOrders", orderController.getAllOrders);
+router.get("/allOrders", orderController.getOrders);
 router.get("/filteredOrders", orderController.getOrdersForSpecificQuery);
-router.get('/:id',adminAuth,orderController.getSpecificOrder);
+router.get("/:id", adminAuth, orderController.getSpecificOrder);
 module.exports = router;
