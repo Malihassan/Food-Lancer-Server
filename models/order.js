22505b5b743cca12994907149f6f42247d420946
Abram Boutros <abramboutros96@gmail.com>
2022-03-14 20:49:39 +0200

    updated order routes

--- models/order.js
+++ models/order.js
@@ -1,41 +1,42 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-
-const orderSchema = mongoose.Schema({
-    buyerId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: "buyer",
-        required: true
-    }, 
-    sellerId:{
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: "seller",
-        required: true
-    },
-    products: [
-    {
-        _id: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "product",
-            required: true
-        },
-        quantity: {
-            type: Number,
-            required: true
-        }
-    }
-    ],
-    totalPrice: {
-        type: Number,
-        required: true
-    },
-    status: {
-        type: String,
-        default: "in progress",
-        enum: ["in progress", "done","cancel"]
-    }
-}, {timestamps: true});
-
+const orderSchema = mongoose.Schema(
+	{
+		buyerId: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: "buyer",
+			required: true,
+		},
+		sellerId: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: "seller",
+			required: true,
+		},
+		products: [
+			{
+				_id: {
+					type: mongoose.SchemaTypes.ObjectId,
+					ref: "product",
+					required: true,
+				},
+				quantity: {
+					type: Number,
+					required: true,
+				},
+			},
+		],
+		totalPrice: {
+			type: Number,
+			required: true,
+		},
+		status: {
+			type: String,
+			default: "in progress",
+			enum: ["in progress", "done", "cancel"],
+		},
+	},
+	{ timestamps: true }
+);
 
 const OrderModel = mongoose.model("order", orderSchema);
 
