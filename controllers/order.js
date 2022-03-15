db3a47efc5bacb8eb88406616dc0096328c5b213
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-15 20:52:12 +0200

    edit in order model and controller to hadel Pagination

--- controllers/order.js
+++ controllers/order.js
@@ -4,162 +4,78 @@ const { path } = require("express/lib/application");
 const { json } = require("express/lib/response");
 
 const getOrdersForSpecificBuyer = (req, res, next) => {
-	const { id } = req.query;
-	orderModel
-		.find({ buyerId: id })
-		.populate({
-			path: "sellerId",
-			select: "userName firstName lastName phone email status gender -_id",
-		})
-		.populate({
-			path: "products",
-			populate: {
-				path: "_id",
-				select:
-					"name description image price addOns reviews avgRate status -_id",
-			},
-		})
-		.then((data) => {
-			if (!data) {
-				return next(new AppError("accountNotFound"));
-			}
-			res.json(data);
-		});
+  const { id } = req.query;
+  orderModel
+    .find({ buyerId: id })
+    .populate({
+      path: "sellerId",
+      select: "userName firstName lastName phone email status gender -_id",
+    })
+    .populate({
+      path: "products",
+      populate: {
+        path: "_id",
+        select:
+          "name description image price addOns reviews avgRate status -_id",
+      },
+    })
+    .then((data) => {
+      if (!data) {
+        return next(new AppError("accountNotFound"));
+      }
+      res.json(data);
+    });
 };
 
-function getOrders(req, res, next) {
-	const { minPrice, maxPrice, orderStatus, id, buyerId, sellerId, page } =
-		req.query;
+const getOrders = async (req, res, next) => {
+  const { minPrice, maxPrice, orderStatus, id, buyerId, sellerId, page=1 } =
+    req.query;
+  const pageSize = 1;
 
-	var count;
-	const pageSize = 1;
+  const minPriceQuery = minPrice ? { totalPrice: { $gte: minPrice } } : {};
+  const maxPriceQuery = maxPrice ? { totalPrice: { $lte: maxPrice } } : {};
+  const orderStatusQuery = orderStatus ? { status: orderStatus } : {};
+  const orderIdQuery = id ? { _id: id } : {};
+  const buyerIdQuery = buyerId ? { buyerId } : {};
+  const sellerIdQuery = sellerId ? { sellerId } : {};
 
-	const minPriceQuery = minPrice ? { totalPrice: { $gte: minPrice } } : {};
-	const maxPriceQuery = maxPrice ? { totalPrice: { $lte: maxPrice } } : {};
-	const orderStatusQuery = orderStatus ? { status: orderStatus } : {};
-	const orderIdQuery = id ? { _id: id } : {};
-	const buyerIdQuery = buyerId ? { buyerId } : {};
-	const sellerIdQuery = sellerId ? { sellerId } : {};
 
-	orderModel
-		.find({
-			$and: [
-				minPriceQuery,
-				maxPriceQuery,
-				orderStatusQuery,
-				orderIdQuery,
-				buyerIdQuery,
-				sellerIdQuery,
-			],
-		})
-		.populate({
-			path: "sellerId",
-			select: "userName firstName lastName phone email status gender -_id",
-		})
-		.populate({
-			path: "buyerId",
-			select: "userName firstName lastName phone email status gender -_id",
-		})
-		.populate({
-			path: "products",
-			populate: {
-				path: "_id",
-				select:
-					"name description image price addOns reviews avgRate status -_id",
-			},
-		})
-		.skip(pageSize * (page - 1))
-		.limit(pageSize)
-		.then(async (data) => {
-			if (
-				minPriceQuery == {} ||
-				maxPriceQuery == {} ||
-				orderStatusQuery == {} ||
-				orderIdQuery == {} ||
-				buyerIdQuery == {} ||
-				sellerIdQuery == {}
-			) {
-				count = data.length;
-				console.log(count, "count");
-				console.log(data, "data");
-			} else {
-				// console.log(data.length);
-				count = await orderModel.count({});
-				console.log(count, "count");
-				console.log(data, "data");
-			}
+  const option = {
+    page: page,
+    limit: pageSize,
+	populate:{
+		path: "sellerId",
+		select: "userName firstName lastName phone email status gender -_id",
+	  },
+	  populate:{
+		path: "buyerId",
+		select: "userName firstName lastName phone email status gender -_id",
+	  },
+	  populate:{
+		path: "products",
+		populate: {
+		  path: "_id",
+		  select:
+			"name description image price addOns reviews avgRate status -_id",
+		}}
+	};
+  const allOrders = await orderModel.paginate(
+    {
+      $and: [minPriceQuery,
+        maxPriceQuery,
+        orderStatusQuery,
+        orderIdQuery,
+        buyerIdQuery,
+        sellerIdQuery,],
+    },
+    option
+  );
+  res.json(allOrders);
 
-			if (!data) {
-				return next(new AppError("accountNotFound"));
-			}
-			res.json({ data, countOfOrders: count });
-		});
-}
-
-// function getOrdersForSpecificQuery(req, res, next) {
-// 	const { minPrice, maxPrice, orderStatus } = req.query;
-
-// 	const minPriceQuery = minPrice ? { totalPrice: { $gte: minPrice } } : {};
-// 	const maxPriceQuery = maxPrice ? { totalPrice: { $lte: maxPrice } } : {};
-// 	const orderStatusQuery = orderStatus ? { status: orderStatus } : {};
-
-// 	orderModel
-// 		.find({
-// 			$and: [minPriceQuery, maxPriceQuery, orderStatusQuery],
-// 		})
-// 		.populate({
-// 			path: "sellerId",
-// 			select: "userName firstName lastName phone email status gender -_id",
-// 		})
-// 		.populate({
-// 			path: "buyerId",
-// 			select: "userName firstName lastName phone email status gender -_id",
-// 		})
-// 		.populate({
-// 			path: "products",
-// 			populate: {
-// 				path: "_id",
-// 				select:
-// 					"name description image price addOns reviews avgRate status -_id",
-// 			},
-// 		})
-// 		.then((data) => {
-// 			if (!data) {
-// 				return next(new AppError("accountNotFound"));
-// 			}
-// 			res.json(data);
-// 		});
-// }
-// const getSpecificOrder = (req, res, next) => {
-// 	const { id } = req.params;
-// 	orderModel
-// 		.findOne({ _id: id })
-// 		.populate({
-// 			path: "buyerId",
-// 			select: "userName firstName lastName phone email status gender -_id",
-// 		})
-// 		.populate({
-// 			path: "sellerId",
-// 			select: "userName firstName lastName phone email status gender -_id",
-// 		})
-// 		.populate({
-// 			path: "products",
-// 			populate: {
-// 				path: "_id",
-// 				select:
-// 					"name description image price addOns reviews avgRate status -_id",
-// 			},
-// 		})
-// 		.then((data) => {
-// 			if (!data) {
-// 				return next(new AppError("accountNotFound"));
-// 			}
-// 			res.json(data);
-// 		});
-// };
+};
 module.exports = {
-	getOrdersForSpecificBuyer,
-	getOrders,
-	// getOrdersForSpecificQuery,
-	// getSpecificOrder,
+  getOrdersForSpecificBuyer,
+  getOrders,
+  // getOrdersForSpecificQuery,
+  // getSpecificOrder,
 };
