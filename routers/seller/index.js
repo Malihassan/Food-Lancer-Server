6ec338a6d273ea1ca9e8f00d248683c32ba5ecbc
BilalAhmedMohammed <bilalelmaestro2019@gmail.com>
2022-03-25 15:37:34 +0200

    edit in category model

--- routers/seller/index.js
+++ routers/seller/index.js
@@ -4,7 +4,7 @@ const accountRouter = require("./account");
 const categoryRouter=require('./category');
 const orderRouter = require('./order');
 
-router.use("/category",categoryRouter )☻;
+router.use("/category",categoryRouter );
 router.use("/account", accountRouter);
 router.use("/product", productRouter);
 router.use('/order',orderRouter)
