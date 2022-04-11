d3c4d55b589052de336054d2dcb23f56726396e6
Malihassan <momenalhendawy@gmail.com>
2022-04-12 00:30:35 +0200

    test notifications

--- routers/buyer/order.js
+++ routers/buyer/order.js
@@ -5,7 +5,7 @@ const buyerController = require("../../controllers/buyer");
 
 const orderController = require("../../controllers/order");
 router.get("/myOrders",buyerAuthentication ,orderController.getOrdersForSpecificBuyer);
-router.post("/add", buyerAuthentication, orderController.addOrder,sellerController.addNotificationToSellerForAddOrder)
+router.post("/add", buyerAuthentication, orderController.addOrder,sellerController.addNotificationToSellerForAddOrder,buyerController.addNotificationToBuyer)
 router.get("/notifications",buyerAuthentication,buyerController.getNotificationsForBuyer)
 router.get('/setOrderNotificationAsReaded',buyerAuthentication,buyerController.setNotificationForOrdersAsReaded);
 
